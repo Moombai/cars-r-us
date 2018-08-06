08c263ef18d90e8bd33ce2c0d518a5b9be6a1f33
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-08-06 17:02:00 +0100

    Add placeholder state and mock-api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: [],
+      sellers: [],
+      sales: []
+    }
+  }
   render() {
     return (
       <div className="App">
