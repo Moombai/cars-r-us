902bac05a8e4bdf2b489f60c0f3e0485dee4a03f
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-08-06 20:46:49 +0100

    Store dealership data in app state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,33 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cars: [],
-      sellers: [],
+      cars: [
+        {
+          "name": "Vauxhall Vectra",
+          "color": "blue",
+          "purchaseValue": 1000
+        },
+        {
+          "name": "Ferrari 458",
+          "color": "red",
+          "purchaseValue": 200000
+        },
+        {
+          "name": "Volkswagen Golf",
+          "color": "white",
+          "purchaseValue": 3000
+        }
+      ],
+      sellers: [
+        {
+          "name": "Will",
+          "lastName": "Brown"
+        },
+        {
+          "name": "Kevin",
+          "lastName": "Yuco"
+        }
+      ],
       sales: []
     }
   }
